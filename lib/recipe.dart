2b04370b53b2d886a62c04f4f627b10eaea52ade
Elliott Record <elliott.record@288group.com>
2021-10-15 09:10:22 +0100

    Added start of recipe class

---
+++ lib/recipe.dart
@@ -0,0 +1,10 @@
+class Recipe {
+  String label;
+  String imageUrl;
+  // TODO: Add servings and ingredients
+
+  Recipe(this.label, this.imageUrl);
+  // TODO Add List<Recipe> here
+}
+
+// TODO: Add Ingredient here
